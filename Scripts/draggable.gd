7419a069f3dd4820b281db1855272290fafa49f3
Kat Berge <kat.n.berge@gmail.com>
2026-08-11 22:19:55 -0500

    adds sprites to ingredients

--- Scripts/draggable.gd
+++ Scripts/draggable.gd
@@ -1,5 +1,9 @@
 extends Node2D
+class_name Draggable
+
+# make sure clickable region is child node of Draggable elements
+@onready var clickable_region: ClickableRegion = $ClickableRegion
 
 func _process(delta: float) -> void:
-	if $ClickableRegion.clicked:
-		position = get_global_mouse_position() + $ClickableRegion.offset
+	if clickable_region.clicked:
+		position = get_global_mouse_position() + clickable_region.offset
