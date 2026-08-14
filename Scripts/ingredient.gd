58d2c64ef658e2e5e9bd2d054dbbdabb3c8321d9
Kat Berge <kat.n.berge@gmail.com>
2026-08-13 22:21:23 -0500

    changes visuals of ingredient when added to recipe

--- Scripts/ingredient.gd
+++ Scripts/ingredient.gd
@@ -4,10 +4,16 @@ class_name Ingredient
 @export var id := -1 
 @export var title := ""
 
+@onready var drag_sprite: Sprite2D = $DraggableSprite
+@onready var in_recipe_sprite: Sprite2D = $InRecipeSprite
+
 # handle behavior when this ingredient is added to current recipe
 func use_ingredient():
 	block_drag = true
 	remove_from_group("ingredient")
+	# handle visuals
+	drag_sprite.visible = false
+	in_recipe_sprite.visible = true
 	
 func _to_string() -> String:
 	return "Ingredient (Title: %s, ID: %d)" % [title, id]
