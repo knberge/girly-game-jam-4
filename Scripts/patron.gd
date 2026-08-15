307cbda21fa8a2cb552ed3db99d213c1f56ca7a2
Daniel <48768975+rowedaniel@users.noreply.github.com>
2026-08-14 19:03:03 -0700

    add patron spawner

--- Scripts/patron.gd
+++ Scripts/patron.gd
@@ -1,12 +1,26 @@
 extends Node2D
 
 @onready var consumerArea = $ConsumerArea
-var added_ingredients := []
+@onready var sprite = $Patron1
+
+@export var images : Array[String] = [
+	"res://Sprites/patron1.jpg"
+]
+
+enum State {UNINITIALIZED, ENTERING, WAITING, EXITING}
+@onready var state : State = State.UNINITIALIZED
 
 func _ready() -> void:
 	consumerArea.consume.connect(_on_consume)
+	
+func _process(delta: float) -> void:
+	pass
 
 func _on_consume(draggable: Draggable):
+	if state != State.WAITING:
+		draggable.drop_bad()
+		return
+
 	print("consuming ", draggable.name)
 	if draggable is ParfaitGlass:
 		rate_glass(draggable)
