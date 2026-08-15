222cab9d4460483e5d2bbac35f3ebd0e326d0c43
Kat Berge <kat.n.berge@gmail.com>
2026-08-15 10:27:07 -0500

    removes old refs to in_recipe sprites for ingredients

--- Scripts/ingredient.gd
+++ Scripts/ingredient.gd
@@ -4,12 +4,9 @@ class_name Ingredient
 @export var id := -1 
 @export var title := ""
 
-@onready var drag_sprite: Sprite2D = $DraggableSprite
-@onready var in_recipe_sprite: Sprite2D = $InRecipeSprite
-
 # handle behavior when this ingredient is added to current recipe
 func use_ingredient():
-	print("used ingredient")
+	print("used ingredient ", title)
 	queue_free()
 	
 func _to_string() -> String:
