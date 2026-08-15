49536b911f56a18bb9539a98fecec3b59c0ca3f5
Daniel <48768975+rowedaniel@users.noreply.github.com>
2026-08-15 11:56:36 -0700

    track LUST, GLUTTONY in ingredient_manager

--- Scripts/managers/ingredient_manager.gd
+++ Scripts/managers/ingredient_manager.gd
@@ -1,9 +1,15 @@
 extends Node
 
 var ingredients : Array[IngredientData] = []
+var hell_layers: Array[HellLayerData] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ingredients = [
 		IngredientData.new(IngredientData.IngredientType.GRANOLA, "", [])
+	]
+	
+	hell_layers = [
+		HellLayerData.new(HellLayerData.Layer.LUST, "res://Sprites/patron_lust.png"),
+		HellLayerData.new(HellLayerData.Layer.GLUTTONY, "res://Sprites/patron_glutton.png"),
 	]
